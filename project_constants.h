8541afb50f2d2ddd4f000bd0c15a863209902455
rodriguespn <pedroneverodrigues@gmail.com>
2020-10-02 13:15:56 +0100

    Project MACROS

--- project_constants.h
+++ project_constants.h
@@ -3,44 +3,63 @@
 
 enum {false, true};
 
-#define SIZE 128
 #define ERROR -1
 #define GN 38 // Group number
 
-#define IP "tejo.tecnico.ulisboa.pt"
+/* APENAS PARA TESTES */
+#define SIZE 128
+#define IP "tejo.tecnico.ulisboa.pt" 
+
 #define PDPORT 57000 // Personal Device default IP
 #define ASPORT 58000 // Authentication System default IP
 
+/* COMMAND LINE FLAGS */
+#define PD_PORT "-d"
+#define AS_IP   "-n"
+#define AS_PORT "-p"
+#define FS_IP   "-m"
+#define FS_PORT "-q"
+#define VERBOSE "-v"
+
 /* COMMUNICATION PROTOCOLS SPECIFICATION */
 // error
-#define PROTOCOL_ERROR "ERR"
+#define PROTOCOL_ERROR      "ERR"
+#define AS_VALIDATION_ERROR "INV"
 
 // status
 #define OK      "OK"
 #define NOT_OK  "NOK"
 
 // PD-AS Protocol
-#define REGISTRATION "REG"
-#define REG_RESPONSE "RRG"
-#define VALIDATION "VLC"
-#define VAL_RESPONSE "RVC"
-#define UNREGISTRATION "UNR"
-#define UNR_RESPONSE "RUN"
+#define REGISTRATION        "REG"
+#define REG_RESPONSE        "RRG"
+#define VALIDATE_USER       "VLC"
+#define VAL_USER_RESPONSE   "RVC"
+#define UNREGISTRATION      "UNR"
+#define UNR_RESPONSE        "RUN"
 
 // User-AS Protocol 
-#define LOGIN "LOG"
-#define LOG_RESPONSE "RLO"
-#define REQUEST "REQ"
-#define REQ_RESPONSE "RRQ"
-#define AUTHENTICATION "AUT"
-#define AUT_RESPONSE "RAU"
+#define LOGIN               "LOG"
+#define LOG_RESPONSE        "RLO"
+#define REQUEST             "REQ"
+#define REQ_RESPONSE        "RRQ"
+#define AUTHENTICATION      "AUT"
+#define AUT_RESPONSE        "RAU"
 
-// User-AS Protocol 
-#define
-#define
-#define
-#define
-#define
+// FS-AS Protocol 
+#define VALIDATE_FILE       "VLD"
+#define VAL_FILE_RESPONSE   "CNF"
 
+// User-FS Protocol 
+#define LIST                "LST"
+#define LIS_RESPONSE        "RLS"
+#define RETRIVE             "RTV"
+#define RET_RESPONSE        "RRT"
+#define UPLOAD              "UPL"
+#define UPL_RESPONSE        "RUP"
+#define DELETE              "DEL"
+#define DEL_RESPONSE        "RDL"
+#define REMOVE              "REM"
+#define REM_RESPONSE        "RRM"
 
 #endif /* PROJECT_CONSTANTS_H */
