8a9f285a7a09f7ae476978ad802900872f6580b0
rodriguespn <pedroneverodrigues@gmail.com>
2020-10-08 18:47:54 +0100

    minor fixes

--- autentication-server/as.c
+++ autentication-server/as.c
@@ -2,7 +2,28 @@
 
 int main(int argc, char const *argv[])
 {
+    int fd,errcode;
+    ssize_t n;
+    socklen_t addrlen;
+    struct addrinfo hints,*res;
+    struct sockaddr_in addr;
+    char buffer[SIZE];
     
-    /* code */
+    // checks if the number of arguments is correct
+    if (wrong_arguments(argc)) {
+        usage();
+        exit(EXIT_FAILURE);
+    }
     exit(EXIT_SUCCESS);
+}
+
+// diplays a message with the correct usage of the file
+void usage() {
+    printf("usage: ./as [-p ASport] [-v]\n");
+    printf("example: ./as -p 58000 -v\n");
+}
+
+// returns true if the arguments given on the command line are on an invalid format, and false otherwise
+int wrong_arguments(int argc) {
+    return !(argc > 0 && argc <= 3);
 }
--- autentication-server/as.h
+++ autentication-server/as.h
@@ -1,7 +1,19 @@
 #ifndef AS_H
 #define AS_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include "../constants.h"
 #include "../functions.c"
+
+void usage();
+int wrong_arguments(int argc);
 
 #endif /* AS_H */
