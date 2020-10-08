30cb52487bdb8b29186ed77ac22f2a323f8975a6
rodriguespn <pedroneverodrigues@gmail.com>
2020-10-08 19:13:05 +0100

    merge master

--- authentication-server/as.h
+++ authentication-server/as.h
@@ -12,12 +12,9 @@
 #include <netdb.h>
 #include "../constants.h"
 #include "../functions.h"
-<<<<<<< HEAD
 
 void usage();
 int wrong_arguments(int argc);
 void parse_arguments(const char* argv[], int size);
-=======
->>>>>>> master
 
 #endif /* AS_H */
