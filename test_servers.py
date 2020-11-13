d7c8434b4190cad10e04987bb940d6106daeeb1a
Pedro Rodrigues <44656907+Rodriguespn@users.noreply.github.com>
2020-11-13 23:05:09 +0000

    Add files via upload

--- test_servers.py
+++ test_servers.py
@@ -4,7 +4,7 @@ import time
 
 as_ip = "193.136.128.109"
 as_port = "58038"
-fs_ip = "193.136.128.109"
+fs_ip = "193.136.128.104"
 fs_port = "59038"
 
 test_host = "tejo.tecnico.ulisboa.pt"
@@ -16,7 +16,7 @@ parent_dir = "./"
 directory = "Script-Tests"
 
 sleep = 1
-timeout = 30
+timeout = 15
   
 path = os.path.join(parent_dir, directory) 
 
@@ -26,33 +26,39 @@ try:
 except FileExistsError:
     print("Directory '{}' already exists".format(path))
 
-number_of_tests = int(input("Insira o numero total de scripts\n"))
-for script in range(1, number_of_tests+1):
+begin_script = int(input("Insira o numero do script onde quer comecar\n"))
+end_script = int(input("Insira o numero do script onde quer acabar\n"))
+count = 0
+for script in range(begin_script, end_script+1):
     url = test_url.format(test_host, test_port, as_ip, as_port, fs_ip, fs_port, script)
     init_time = time.time()
     print("GET {}".format(url))
     while True:
-        curr_time = time.time() - init_time
         try:
             response = requests.get(url)
             print("Status code: {}".format(response.status_code))
+            
+            if response.status_code == 200: # ok
+                content = response.content
+
+                with open('{}/RC-script_{}.html'.format(path, script), 'wb') as f:
+                    f.write(content)
+                count += 1
+
+            else: 
+                print("Got status code {}... skipping script {}".format(response.status_code, script))
+            break
 
         except Exception as e:
             #print("Exception {}".format(e))
             #print("Something went wrong... skipping script {}".format(script))
             time.sleep(sleep)
-            continue
-            
-        if curr_time > timeout:
-            print("Timeout... skipping script {}".format(script))
-            break
 
-        if response.status_code == 200: # ok
-            content = response.content
+        finally:
+            curr_time = time.time() - init_time
+            print(curr_time)
+            if curr_time > timeout:
+                print("Timeout... skipping script {}".format(script))
+                break
 
-            with open('{}/RC-script_{}.html'.format(path, script), 'wb') as f:
-                f.write(content)
-
-        else: 
-            print("Got status code {}... skipping script {}".format(response.status_code, script))
-        break
+print("{} tests completed".format(count))
